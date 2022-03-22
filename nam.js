b0cc9824ef9d8531f644c000392623530e4ccde4
omar abed <omar.aabed112@gmail.com>
2022-03-21 21:49:08 -0800

    Comment

---
+++ nam.js
@@ -0,0 +1,96 @@
+/*
+const num1 = +prompt("Please enter the first number");
+let x;
+if(num1>=60) x=true;
+else x=false;
+if(x) alert("t");
+else alert("f");
+/*
+const num2 = prompt("Please enter +-/*");
+const num3 = +prompt("Please enter the third number");
+let sum;
+
+if(num2=='+')
+{
+    sum=num1+num3;
+}
+else if (num2=='-')
+{
+    sum=num1-num3;
+}
+else if (num2=='*')
+{
+    sum=num1*num3;
+}
+else if (num2=='/')
+{
+    sum=num1/num3;
+}
+alert(sum);
+
+switch(num2)
+{
+    case "+":
+        sum=num1+num3;
+        break;
+    case "-":
+        sum=num1-num3;
+        break;
+    case "*":
+        sum=num1*num3;
+        break;
+    case "/":
+        sum=num1/num3;
+        break;
+                                
+}
+alert(sum);
+*/
+/*
+if(num1>num2){
+    if(num1>num3)
+    {
+        alert(`max is: ${num1}`);
+    }
+    else{
+        alert(`max is: ${num3}`);
+    }
+}
+else{
+    if(num2>num3)
+    {
+        alert(`max is: ${num2}`);
+    }
+    else{
+        alert(`max is: ${num3}`);
+    }
+}
+
+switch(num3){
+    case 1:
+        alert(num1+num2);
+        break;
+    
+    case 2:
+        alert(num1-num2);
+        break;
+   
+    case 3:
+        alert(num1*num2);
+        break;
+    
+    case 4:
+        alert(num1/num2);
+    
+}
+
+function sum(a){
+    let sum=1;
+    for(let i=a;i>0;i--)
+    {
+        sum*=i;
+    }
+    return sum;
+}
+document.write(sum(num1));
+*/
